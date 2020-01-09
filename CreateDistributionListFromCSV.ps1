ad3a50a68a2c9984ec490bf5d9d314e8d10aef4d
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2020-01-09 14:07:49 +0000

    added create distribution list from csv and import distribution list

---
+++ CreateDistributionListFromCSV.ps1
@@ -0,0 +1,23 @@
+﻿$lists = import-csv “C:\Users\ShaneCunningham\Documents\scripts\disGroup.csv”
+foreach ($list in $lists ){
+$name = ($list."Name".Trim())
+$displayName = ($list."DisplayName".Trim())
+$alias = ($list."Alias".Trim())
+$alias = $alias.replace(' ','')
+
+$externalEmail = ($list."PrimarySMTPAddress".Trim()) + "@finegael.ie"
+$externalEmail = $externalEmail.replace(' ','')
+Write-host $name + "Email " + $displayName + " " + $alias
+Write-Host $externalEmail
+
+#New-MailContact -ExternalEmailAddress  $externalEmail -Name $externalEmail
+
+New-DistributionGroup -Name $name -DisplayName $displayname –Alias $alias -PrimarySmtpAddress $externalEmail
+
+}
+
+
+
+#
+#Import-Csv "C:\Users\v-9soaway\Desktop\groups.csv" | foreach {New-DistributionGroup -Name $_.name -DisplayName $_.displayname –Alias 
+#$_.alias -PrimarySmtpAddress $_.PrimarySmtpAddress -RequireSenderAuthenticationEnabled ([boolean] $_.RequireSenderAuthenticationEnabled)}
---
+++ importdistribution.ps1
@@ -0,0 +1,28 @@
+﻿#$UserCredential = Get-Credential
+
+#$O365Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+#Import-PSSession $O365Session
+
+
+$contactList = import-csv “C:\Users\ShaneCunningham\Desktop\Email lists\Farming.csv”
+
+foreach ($contact in $contactList){
+#$fn = ($contact."First Name".Trim())
+#$ln = ($contact."Last Name".Trim())
+#$mn = ($contact."Middle Name".Trim())
+$externalEmail = ($contact."E-mail Address".Trim())
+$fullname = $fn + "." + $mn + "." + $ln
+#Write-host $fullname + "Email " + $externalEmail
+#Write-Host $externalEmail
+
+Add-DistributionGroupMember -Identity "test" -Member $externalEmail
+
+
+}
+
+
+
+#$fn = $_."First Name" $ln = $_."Last Name" $full = $fn + " " + $ln
+
+ #import-CSV "C:\Users\ShaneCunningham\Documents\scripts\test.csv" | Foreach-Object { Add-DistributionGroupMember -Identity "test" -Member $_.Member } -WhatIf
