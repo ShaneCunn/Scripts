ffa4d68a10bf6f98252d17c99bcf4fc695391716
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2020-10-20 08:15:32 +0100

    added unlicense user list and get members of group and owners

---
+++ getlistof unlicenesed users.ps1
@@ -0,0 +1,9 @@
+﻿ Install-Module -Name AzureAD
+
+$credential = Get-Credential
+
+Connect-AzureAD -Credential $credential
+
+#Get-AzureAdUser | ForEach{ $licensed=$False ; For ($i=0; $i -le ($_.AssignedLicenses | Measure).Count ; $i++) { If( [string]::IsNullOrEmpty(  $_.AssignedLicenses[$i].SkuId ) -ne $True) { $licensed=$true } } ; If( $licensed -eq $false) { Write-Host $_.UserPrincipalName} }
+
+Get-MsolUser -UnlicensedUsersOnly | export-csv c:\reports\Unlicensed_Users.CSV
