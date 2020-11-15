df1fcf168aff93e4e1b1f04ffe6918717ee130b1
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2020-11-15 11:58:03 +0000

    created setupmachine script

---
+++ setupmachine.ps1
@@ -0,0 +1,16 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+Write-Host "Starting script"
+New-Item -ItemType directory -Path C:\Sleepless
+Set-Location -Path C:\Sleepless
+Copy-Item 'C:\Users\ShaneCunningham\Documents\GitHub\Scripts\uninstallcrap.ps1' 'C:\Sleepless\uninstallcrap.ps1'
+Get-Location
+callCleanBloatware
+
+function callCleanBloatware(){
+Write-Host "Starting Bloatware script" 
+
+& '.\uninstallcrap.ps1'
+#Get-ChildItem
+
+}
