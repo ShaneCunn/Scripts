d5e735949ed6141ebd7efef00b2c06d5ec389d05
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2021-02-05 17:05:27 +0000

    fixed error with code order and added ninite

--- setupmachine.ps1
+++ setupmachine.ps1
@@ -10,167 +10,161 @@ $Password = "password123!"
 $group = "Administrators"
 
 $adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
-$existing = $adsi.Children | where {$_.SchemaClassName -eq 'user' -and $_.Name -eq $Username }
+$existing = $adsi.Children | where { $_.SchemaClassName -eq 'user' -and $_.Name -eq $Username }
 
 $pass = Read-Host 'What is your password?' -AsSecureString
 
 #Write-Host "Starting script"
 
 
-if(!$folderExists){
-New-Item -ItemType directory -Path C:\Sleeplessrenamepc
+if (!$folderExists) {
+  New-Item -ItemType directory -Path C:\Sleepless
 }
-else{
-#Write-Host "Folder exists"
+else {
+  #Write-Host "Folder exists"
 }
 
 Set-Location -Path C:\Sleepless
 
-callCleanBloatware
+function renamePC() {
 
+  $orginalPCname = $env:COMPUTERNAME
 
-function renamePC(){
+  [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+  $compName = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the PC name", "Name", $orginalPCname )
+  "Your name is $compName"
 
-$orginalPCname = $env:COMPUTERNAME
-
-[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
-$compName = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the PC name", "Name", $orginalPCname )
-"Your name is $compName"
-
-Rename-Computer -NewName $compName
+  Rename-Computer -NewName $compName
 }
 renamePC
 
+function cleanmachine() {
+  <#
+  This is a helpful script that will remove the default Windows 10 garbage from your computer
+  
+  * Comment out any apps that you might want to keep
+  * Don't forget to run as admin
+#>
+  #Get-ExecutionPolicy;
+  #Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
 
-function callCleanBloatware(){
-Write-Host "Starting Bloatware script" 
 
-cleanmachine
+  # 3d Builder
+  Get-AppxPackage *3dbuilder* | Remove-AppxPackage
 
-Write-Host "Finished bloatware script"
+  # Windows Alarms
+  Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+
+  # Windows Calculator
+  #Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+
+  # Windows Communications Apps
+  Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+
+  # Windows Camera
+  #Get-AppxPackage *windowscamera* | Remove-AppxPackage
+
+  # Office Hub
+  #Get-AppxPackage *officehub* | Remove-AppxPackage
+
+  # Skype App
+  Get-AppxPackage *skypeapp* | Remove-AppxPackage
+
+  # Get Started
+  Get-AppxPackage *getstarted* | Remove-AppxPackage
+
+  # Zune Music
+  Get-AppxPackage *zunemusic* | Remove-AppxPackage
+
+  # Windows Maps
+  Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+
+  # Solitaire Collection
+  Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+
+  # Bing Finance
+  Get-AppxPackage *bingfinance* | Remove-AppxPackage
+
+  # Zune Video
+  Get-AppxPackage *zunevideo* | Remove-AppxPackage
+
+  # Bing News
+  Get-AppxPackage *bingnews* | Remove-AppxPackage
+
+  # OneNote
+  Get-AppxPackage *onenote* | Remove-AppxPackage
+
+  # People
+  #Get-AppxPackage *people* | Remove-AppxPackage
+
+  # Windows Phone
+  Get-AppxPackage *windowsphone* | Remove-AppxPackage
+
+  # Photos
+  Get-AppxPackage *photos* | Remove-AppxPackage
+
+  # Windows Store
+  #Get-AppxPackage *windowsstore* | Remove-AppxPackage
+
+  # Bing Sports
+  Get-AppxPackage *bingsports* | Remove-AppxPackage
+
+  # Sound Recorder
+  Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+
+  # Bing Weather
+  Get-AppxPackage *bingweather* | Remove-AppxPackage
+
+  # Xbox App
+  Get-AppxPackage *xboxapp* | Remove-AppxPackage
+
+  <#
+# Reversal Script (Run this line to install the apps again)
+Get-AppxPackage -AllUsers| Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)\AppXManifest.xml”}
+#>
+
 }
+
+function callCleanBloatware() {
+  Write-Host "Starting Bloatware script" 
+
+  cleanmachine
+
+  Write-Host "Finished bloatware script"
+}
+
 
 callSetupLocalAdmin
 
-function callSetupLocalAdmin(){
-Write-host "Calling local admin script"  -ForegroundColor Green
+function callSetupLocalAdmin() {
+  Write-host "Calling local admin script"  -ForegroundColor Green
 
 
-if ($existing -eq $null) {
+  if ($existing -eq $null) {
 
-   Write-Host "Creating new local user $Username."
+    Write-Host "Creating new local user $Username."
     & NET USER $Username $pass /add /y /expires:never
     
-   Write-Host "Adding local user $Username to $group."
+    Write-Host "Adding local user $Username to $group."
     & NET LOCALGROUP $group $Username /add
 
-}
-else {
-   Write-Host "Setting password for existing local user $Username."
+  }
+  else {
+    Write-Host "Setting password for existing local user $Username."
     $existing.SetPassword($pass)
-}
+  }
 
-Write-Host "Ensuring password for $Username never expires."
-& WMIC USERACCOUNT WHERE "Name='$Username'" SET PasswordExpires=FALSE
+  Write-Host "Ensuring password for $Username never expires."
+  & WMIC USERACCOUNT WHERE "Name='$Username'" SET PasswordExpires=FALSE
 }
 
 Netplwiz.exe
 
 installOffice365
 
-function installOffice365(){
-Write-host "Starting installer for Office 365"  -ForegroundColor Red -BackgroundColor Yellow
+function installOffice365() {
+  Write-host "Starting installer for Office 365"  -ForegroundColor Red -BackgroundColor Yellow
 
-Start-Process C:\Sleepless\OfficeSetup.exe -Wait
-
-}
-
-
-
-
-function cleanmachine(){
-<#
-  This is a helpful script that will remove the default Windows 10 garbage from your computer
-  
-  * Comment out any apps that you might want to keep
-  * Don't forget to run as admin
-#>
-#Get-ExecutionPolicy;
-#Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
-
-
-# 3d Builder
-Get-AppxPackage *3dbuilder* | Remove-AppxPackage
-
-# Windows Alarms
-Get-AppxPackage *windowsalarms* | Remove-AppxPackage
-
-# Windows Calculator
-#Get-AppxPackage *windowscalculator* | Remove-AppxPackage
-
-# Windows Communications Apps
-Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
-
-# Windows Camera
-#Get-AppxPackage *windowscamera* | Remove-AppxPackage
-
-# Office Hub
-#Get-AppxPackage *officehub* | Remove-AppxPackage
-
-# Skype App
-Get-AppxPackage *skypeapp* | Remove-AppxPackage
-
-# Get Started
-Get-AppxPackage *getstarted* | Remove-AppxPackage
-
-# Zune Music
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
-
-# Windows Maps
-Get-AppxPackage *windowsmaps* | Remove-AppxPackage
-
-# Solitaire Collection
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
-
-# Bing Finance
-Get-AppxPackage *bingfinance* | Remove-AppxPackage
-
-# Zune Video
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
-
-# Bing News
-Get-AppxPackage *bingnews* | Remove-AppxPackage
-
-# OneNote
-Get-AppxPackage *onenote* | Remove-AppxPackage
-
-# People
-#Get-AppxPackage *people* | Remove-AppxPackage
-
-# Windows Phone
-Get-AppxPackage *windowsphone* | Remove-AppxPackage
-
-# Photos
-Get-AppxPackage *photos* | Remove-AppxPackage
-
-# Windows Store
-#Get-AppxPackage *windowsstore* | Remove-AppxPackage
-
-# Bing Sports
-Get-AppxPackage *bingsports* | Remove-AppxPackage
-
-# Sound Recorder
-Get-AppxPackage *soundrecorder* | Remove-AppxPackage
-
-# Bing Weather
-Get-AppxPackage *bingweather* | Remove-AppxPackage
-
-# Xbox App
-Get-AppxPackage *xboxapp* | Remove-AppxPackage
-
-<#
-# Reversal Script (Run this line to install the apps again)
-Get-AppxPackage -AllUsers| Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)\AppXManifest.xml”}
-#>
+  Start-Process C:\Sleepless\OfficeSetup.exe -Wait
 
 }
