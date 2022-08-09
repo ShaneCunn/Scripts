2e66b7440e6dd3282f33b16a9ea83ba7407aac09
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2022-08-09 19:15:49 +0100

    added check of weather the program was already installed

--- Installcommonprograms.ps1
+++ Installcommonprograms.ps1
@@ -1,3 +1,50 @@
 ﻿Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install notepadplusplus teamviewer 7zip googlechrome zoom -y
+$localprograms = choco list --localonly
+if ($localprograms -like "*googlechrome*")
+{
+    choco upgrade googlechrome
+}
+Else
+{
+    choco install googlechrome -y
+}
+
+if ($localprograms -like "*notepadplusplus*")
+{
+    choco upgrade notepadplusplus
+}
+Else
+{
+    choco install notepadplusplus -y
+}
+
+
+if ($localprograms -like "*teamviewer*")
+{
+    choco upgrade teamviewer
+}
+Else
+{
+    choco install teamviewer -y
+}
+
+
+if ($localprograms -like "*7zip*")
+{
+    choco upgrade 7zip
+}
+Else
+{
+    choco install 7zip -y
+}
+
+
+if ($localprograms -like "*zoom*")
+{
+    choco upgrade zoom
+}
+Else
+{
+    choco install zoom -y
+}
