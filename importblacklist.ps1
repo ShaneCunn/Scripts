3db0d2653bd4e0315930b57499168357dd224bd7
Shane Cunningham <ShaneCunn@users.noreply.github.com>
2022-08-11 12:08:54 +0100

    first draft of blacklist script

---
+++ importblacklist.ps1
@@ -0,0 +1,21 @@
+﻿##Import-Module ExchangeOnlineManagement
+##Connect-ExchangeOnline
+##foreach ($content in (get-content “D:\blacklist_addresses.csv”)){$temp=(get-transportrule “Blacklist”).From;$temp+=$content;set-transportrule “Blacklist” -From $temp}
+
+
+##Get-Mailbox -ResultSize unlimited
+
+##Get-HostedContentFilterPolicy "test blocklist"
+
+
+ 
+$emails =  "C:\Users\ShaneCunninghamSleep\Sleepless Server Solutions Ltd\Projects - Documents\Chanelle - Public Folders Migration\testlist.csv" # EDIT THIS
+ 
+$count = 1
+foreach ($content in ($total = get-content $emails)){
+    $totalcount = $total.count
+    Set-HostedContentFilterPolicy -Identity "test blocklist" –BlockedSenders @{add=$content}
+    write-host "Added $count entries of $totalcount : $content"
+    $count += 1
+}
+
